e48d02845e81f767e73d4f4a77694f13ee09f2cf
202chaz <chaz.haskins@gmail.com>
2018-01-07 13:43:09 -0500

    cap file update

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/202chaz/dmvevents.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/rails/rails_project"
+set :deploy_to, "/home/rails/dmvevents"
 set :rails_env, 'production'
 
 # Default value for :format is :airbrussh.
