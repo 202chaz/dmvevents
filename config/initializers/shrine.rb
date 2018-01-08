120a51032c7e9f25c1c7a1f3b4e9471942411506
202chaz <chaz.haskins@gmail.com>
2018-01-08 11:41:39 -0500

    Shrine updates

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,9 @@
 require "shrine/storage/s3"
 
 s3_options = {
-  access_key_id: Rails.application.secrets.digitalocean_spaces_key,
-  secret_access_key: Rails.application.secrets.digitalocean_secret_key,
-  bucket: Rails.application.secrets.digitalocean_bucket_key,
+  access_key_id: ENV['DIGITALOCEAN_SPACES_KEY'],
+  secret_access_key: ENV['DIGITALOCEAN_SECRET_SECRET'],
+  bucket: ENV['DIGITALOCEAN_BUCKET_BUCKET'],
   endpoint: 'https://nyc3.digitaloceanspaces.com',
   region: 'nyc3'
 }
